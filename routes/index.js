57de0fe9a7f09062ed1136170bd4b951a09b4e93
AntonioSalazar <antonio.salazar.emrsn@gmail.com>
2019-03-17 14:42:39 -0600

    finished independent articles endpoints

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,9 @@ const router  = express.Router();
 const User    = require("../models/user");
 const newsIndepentArticle = require("../models/newsIndependent");
 const uploadCloud = require("../config/cloudinary");
-const multer = require("multer");
-const upload = multer({dest: "uploads/"});
 
 router.post('/add_article', uploadCloud.single("photo"),(req, res, next) => {
-
   const {author, newsContent} = req.body
-  console.log(req.body);
   newsIndepentArticle.create({
     author ,
     newsContent ,
@@ -24,5 +20,41 @@ router.post('/add_article', uploadCloud.single("photo"),(req, res, next) => {
   })
   .catch( err => next(res.status(400).json(err)))
 });
+
+router.get("/independent_articles", (req, res, next ) => {
+  newsIndepentArticle.find()
+  .then(allTheArticles => {
+    res.status(200).json(
+      allTheArticles
+    )
+  })
+  .catch(err => next(res.status(400).json(err)))
+})
+
+router.patch("/independent_articles/edit/:id", (req, res, next) => {
+  newsIndepentArticle.findByIdAndUpdate({"_id": req.params.id}, req.body, { new: true})
+  .then(updatedArticle => {
+    res.json({
+      message: `article with the id ${req.params.id} has been updated`
+    })
+  })
+  .catch(err => next(res.status(400).json(err)))
+})
+
+router.delete("/independent_article/delete/:id", (req, res, next) => {
+  newsIndepentArticle.findByIdAndDelete(req.params.id)
+  .then(deletedArticle => {
+    res.status(200).json({
+      message : `Article with id ${req.params.id} has been deleted`
+    })
+  })
+  .catch(err => next(res.status(400).json(err)))
+})
+
+router.get("/independent_articles/:id", (req, res, next) => {
+  newsIndepentArticle.findById(req.params.id)
+  .then(singleArticle => res.status(200).json(singleArticle))
+  .catch(err => next(res.status(400).json(err)))
+})
 
 module.exports = router;

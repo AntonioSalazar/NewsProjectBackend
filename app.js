a7f8635297ec8fa865710306a97bd8eda9deef52
Salazar <antonio.salazar@oliverwyman.com>
2019-04-11 10:33:26 -0500

    still fixing db connection

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const cors            = require('cors');
 require("./config/passport")
 
 mongoose
-  .connect(process.env.MONGODB_URI, {useNewUrlParser : true})
+  .connect('process.env.MONGODB_URI', {useNewUrlParser : true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })

aeb86e930d41afcce15b7595d40dbb185f7f01e7
Salazar <antonio.salazar@oliverwyman.com>
2019-04-11 09:27:32 -0500

    modifications to environment variables

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const cors            = require('cors');
 require("./config/passport")
 
 mongoose
-  .connect('mongodb://localhost/newsprojectbackend', {useNewUrlParser : true})
+  // .connect('mongodb://localhost/newsprojectbackend', {useNewUrlParser : true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })

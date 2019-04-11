02c432760108f2fad664a67fa82d1b62487bbabd
Salazar <antonio.salazar@oliverwyman.com>
2019-04-11 09:54:01 -0500

    deploy

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ const cors            = require('cors');
 require("./config/passport")
 
 mongoose
-  // .connect('mongodb://localhost/newsprojectbackend', {useNewUrlParser : true})
-  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
+  .connect(process.env.MONGO_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
